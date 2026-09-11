e9fde668414766e28e8e9664aa945d603a855b5e
bruno2001ka-coder <bruno2001ka@gmail.com>
2026-09-11 15:34:14 -0300

    chore: renovar cache da malha viaria completa

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quintal-clean-v25';
+const CACHE='quintal-clean-v26';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './src/main.js','./src/GamePhysics.js','./src/Rodas.js','./src/TestTrack.js','./src/Profiler.js'

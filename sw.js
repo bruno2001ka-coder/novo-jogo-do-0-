1a1c4a155a08a2a34c7f35e6b0a0cfc7de148d45
bruno2001ka-coder <bruno2001ka@gmail.com>
2026-09-11 15:49:15 -0300

    chore: renovar cache apos correcao das ruas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quintal-clean-v26';
+const CACHE='quintal-clean-v27';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './src/main.js','./src/GamePhysics.js','./src/Rodas.js','./src/TestTrack.js','./src/Profiler.js'

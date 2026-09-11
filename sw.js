5615e133744cdc5f5701db485d88c9c7ff521824
bruno2001ka-coder <bruno2001ka@gmail.com>
2026-09-11 17:07:13 -0300

    fix: refresh PWA cache for build 32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quintal-clean-v31';
+const CACHE='quintal-clean-v32';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './src/main.js','./src/GamePhysics.js','./src/Rodas.js','./src/TestTrack.js','./src/Profiler.js'

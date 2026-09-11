6a908ab4478002f5858fe01bafa98ca7b0f61caa
bruno2001ka-coder <bruno2001ka@gmail.com>
2026-09-11 16:21:12 -0300

    fix: refresh PWA service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quintal-clean-v28';
+const CACHE='quintal-clean-v29';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./icon.svg',
   './src/main.js','./src/GamePhysics.js','./src/Rodas.js','./src/TestTrack.js','./src/Profiler.js'
